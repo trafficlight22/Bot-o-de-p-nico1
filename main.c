cf450ec8a6a5a7062f1cb0000ed4c4f24508781a
trafficlight22 <semaforoa36@gmail.com>
2026-03-19 13:31:20 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,34 @@
+#include <stdio.h>
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+// Definições
 #define BUTTON_GPIO 4
-#define DEBOUNCE_LIMIT 500
+#define LED_STATUS  2
+#define BUZZER      GPIO_NUM_23
+
+#define DEBOUNCE_TIME_MS 50
+#define HOLD_TIME_MS     6000  // 6 segundos
+
+int ativado = 0;
+
+// Sirene
+void tocarSirene() {
+    gpio_set_level(BUZZER, 1);
+}
+
+void pararSirene() {
+    gpio_set_level(BUZZER, 0);
+}
+
+void enviarMensagem() {
+    printf("SOS ATIVADO\n");
+}
 
 void app_main(void)
 {
-    int counter = 0;
+    // Configura botão com pull-up
     gpio_config_t io_conf = {
         .pin_bit_mask = (1ULL << BUTTON_GPIO),
         .mode = GPIO_MODE_INPUT,
@@ -15,28 +36,56 @@ void app_main(void)
     };
     gpio_config(&io_conf);
 
+    // LED e buzzer
+    gpio_set_direction(LED_STATUS, GPIO_MODE_OUTPUT);
+    gpio_set_direction(BUZZER, GPIO_MODE_OUTPUT);
+
     while (1)
     {
-        if (gpio_get_level(BUTTON_GPIO) == 0)
+        // Detecta botão pressionado
+        if (gpio_get_level(BUTTON_GPIO) == 0 && !ativado)
         {
-            counter++;
-            printf("%d\n",counter);
-            if (counter >= DEBOUNCE_LIMIT)
-            {
-                printf("Pressionamento validado\n");
-                counter = 0;
+            vTaskDelay(pdMS_TO_TICKS(DEBOUNCE_TIME_MS));
 
+            if (gpio_get_level(BUTTON_GPIO) == 0)
+            {
+                int tempo = 0;
+
+                // ⏱️ Espera segurar botão
                 while (gpio_get_level(BUTTON_GPIO) == 0)
+                {
                     vTaskDelay(pdMS_TO_TICKS(10));
+                    tempo += 10;
+
+                    if (tempo >= HOLD_TIME_MS)
+                    {
+                        ativado = 1;
+
+                        gpio_set_level(LED_STATUS, 1);
+                        tocarSirene();
+                        enviarMensagem();
+
+                        break;
+                    }
+                }
             }
         }
-        else
+        if (ativado)
         {
-            counter = 0;
+            if (gpio_get_level(BUTTON_GPIO) == 0)
+            {
+                vTaskDelay(pdMS_TO_TICKS(DEBOUNCE_TIME_MS));
+
+                if (gpio_get_level(BUTTON_GPIO) == 0)
+                {
+                    ativado = 0;
+
+                    gpio_set_level(LED_STATUS, 0);
+                    pararSirene();
+                }
+            }
         }
 
         vTaskDelay(pdMS_TO_TICKS(10));
     }
-
 }
-
